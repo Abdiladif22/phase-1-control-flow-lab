441f4501e93604e048064da4ccdce07031132cb1
Abdiladi22 <abdiladifali0@gmail.com>
2023-03-20 23:08:15 +0300

    compelet assignment

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
-}
-
-function ternaryCheckCity(){
+  if(distance<=400 ){
+return 'This one is on me!';
+  }
+  else if(distance > 2000 && distance <2500){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else{
+    return 'No can do.';
+  }}function ternaryCheckCity(city){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
+ return city === 'NYC'? "Ok, sounds good.": "No go."}function switchOnCharmFromTip(tip){
   // Write your code here!
-}
+  if(tip === "generous"){
+    return "Thank you so much.";
+  }
+  else if(tip ===  "not as generous" ){
+    return "Thank you.";
+  }
+  else{
+    return "Bye.";
+  }}
